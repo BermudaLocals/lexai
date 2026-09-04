0d23ea61a7b4360ab720333415aadb4a91d440ec
Kush COO <kush@dollardoubleempire.com>
2026-09-04 19:57:30 +0200

    fix 502 use bcrypt not bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const { pool } = require('../db');
 const router = express.Router();
 router.post('/login', async (req,res)=>{
